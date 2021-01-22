bbbcaa18ac077a2100e2c5d9e05d9794ae0e0f91
Felipe Garcia Alves <47622464+felipAlves@users.noreply.github.com>
2021-01-22 10:01:40 -0300

    Retornando filhos da div pai com seus respectivos atributos

--- dist/index.js
+++ dist/index.js
@@ -4,45 +4,59 @@
      * 
      * @param {*} component - Html em forma de objeto para ser convertido  
      */
-    function makeComponent(component, componentName) {
+    function makeComponent(component) {
         this.preFinalHtml = []
         
         const StructurePreFinalHtml = Object.keys(component).map(key => {
             if(component[key].children) {
                 let nowObjetc = component[key].children
+                console.log(key)
                 let found = true
                 let childrens = []    
                 
-                Object.keys(component[key].attributes).map(keyMainContainer => {
-                    this.preFinalHtml[key] = [keyMainContainer]
-                    this.preFinalHtml[key][keyMainContainer] = `${keyMainContainer}='${component[key].attributes[keyMainContainer]}'`
+                Object.keys(nowObjetc).map(keyMainContainer => {
+                    // return console.log(keyMainContainer)
+                    Object.keys(nowObjetc[keyMainContainer].attributes).map(attribute => {
+                        console.log(keyMainContainer, attribute, nowObjetc[keyMainContainer].attributes[attribute])
+                        this.preFinalHtml[key] = [keyMainContainer]
+                        this.preFinalHtml[key][keyMainContainer] = [attribute]
+                        this.preFinalHtml[key][keyMainContainer][attribute] = `${attribute}='${nowObjetc[keyMainContainer].attributes[attribute]}'`
+                    })
+                    
+                    
+                    this.preFinalHtml[key][keyMainContainer] = nowObjetc[keyMainContainer].children 
+                    console.log(this.preFinalHtml)
                 })
 
-                do {
+                // Fazer o mesmo funcionamento do código a cima com todos filhos de todos elementos num laço do while
+                // nowObjetct na primeira vez será todos filhos do elemento div
+
+                // do {
                    
-                    Object.keys(nowObjetc).map((newKey) => {
-                        childrens[key] = {
-                            elementName: newKey,
-                            children: nowObjetc[newKey]
-                        }
+                //     Object.keys(nowObjetc).map((newKey) => {
+                //         childrens[key] = {
+                //             elementName: newKey,
+                //             children: nowObjetc[newKey]
+                //         }
 
-                        Object.keys(nowObjetc[newKey].attributes).map(keyNowObject => {
-                            this.preFinalHtml[newKey] = [keyNowObject]
-                            this.preFinalHtml[newKey][keyNowObject] = `${keyNowObject}=${nowObjetc[newKey].attributes[keyNowObject]}'`
-                        })
+                //         Object.keys(nowObjetc[newKey].attributes).map(keyNowObject => {
+                //             this.preFinalHtml[newKey] = [keyNowObject]
+                //             this.preFinalHtml[newKey][keyNowObject] = ['value']
+                //             this.preFinalHtml[newKey][keyNowObject]['value'] = `${keyNowObject}='${nowObjetc[newKey].attributes[keyNowObject]}'`
+                //         })
 
-                        nowObjetc = nowObjetc[newKey]
+                //         nowObjetc = nowObjetc[newKey]
 
-                        if(!nowObjetc[newKey]) {
-                            found = false
-                        }
-                    })
+                //         if(!nowObjetc[newKey]) {
+                //             found = false
+                //         }
+                //     })
 
-                } while (found)
+                // } while (found)
 
                 return this.preFinalHtml
-            }
 
+            }
             
         });
 
@@ -67,9 +81,19 @@
                         class: 'title'
                     }
                 },
+                h2: {
+                    children: {
+                        p: {
+                            content: 'ola'
+                        }
+                    },
+                    attributes: {
+                        class: 'title'
+                    }
+                },
             }
         }
-    }, 'MainComponent')
+    },)
 
     console.log(response)
 
